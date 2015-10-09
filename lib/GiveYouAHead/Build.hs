f97b4dc04451474c625e15b05296746786a0247f
Qinka <qinka@live.com>
2015-10-09 21:55:58 +0800

    fix bugs and change the .cabal file

--- lib/GiveYouAHead/Build.hs
+++ lib/GiveYouAHead/Build.hs
@@ -8,11 +8,11 @@ module GiveYouAHead.Build
     build
     ) where
 
-      import System.Directory(getDirectoryContents)
+      import System.Directory(getDirectoryContents,doesFileExist)
       import System.Process(createProcess,shell,waitForProcess)
       --import GHC.IO.Exception()
 
-      import GiveYouAHead.Common(getDataDir,writeF)
+      import GiveYouAHead.Common(getDataDir,writeF,readF)
       import GiveYouAHead.Template(getCM,getTemplate)
       import GiveYouAHead.Build.File(getFilesList)
       import Data.GiveYouAHead(findKey,toText,Switch(..),USettings(..))
@@ -23,17 +23,27 @@ module GiveYouAHead.Build
             -> IO()
 
       build tp list' = do
+        ignore <- readIgnore ".gyah/build.ignore"
         us' <- getDataDir >>= (getUSettings.(++"/usettings"))
         let (Just us) =us'
         cc <- getDirectoryContents "."
         cm <- getCM
         bt <- getTemplate $ if null tp then "build.default" else "build." ++ tp
         btstep <- getTemplate $ if null tp then "build.step.default" else "build.step" ++ tp
-        writeF (".makefile" ++ findKey cm "ShellFileBack" ) $ (concat.toText (cm' cm btstep cc)) bt
-        (_,_,_,pHandle) <- createProcess $ shell $ sysShell us ++ ".makefile" ++ findKey cm "ShellFileBack"
+        writeF (".makefile" ++ findKey cm "ShellFileBack" ) $ (concat.toText (cm' cm btstep cc ignore)) bt
+        (_,_,_,pHandle) <- createProcess $ shell $ sysShell us ++ " .makefile" ++ findKey cm "ShellFileBack"
         _ <- waitForProcess pHandle
         return ()
         where
-          list ccontents cmdMap =
-            if null list' then getFilesList (findKey cmdMap "FE") ccontents else getFilesList (findKey cmdMap "FE") list'
-          cm' cm bts cc= (On,"build",unlines.map (\x->concat $ toText ((On,"file",x):cm) bts) $ list cc cm):cm
+          list ccontents cmdMap ignore=
+            if null list' then
+              delIgnore (lines ignore) $ getFilesList (findKey cmdMap "FE") ccontents
+              else getFilesList (findKey cmdMap "FE") $ map ((++ findKey cmdMap "numRight").(findKey cmdMap "numLeft" ++)) list'
+          cm' cm bts cc ignore= (On,"build",unlines.map (\x->concat $ toText ((On,"file",x):cm) bts) $ list cc cm ignore):cm
+          delIgnore _ [] = []
+          delIgnore is (x:xs)
+            | x `elem` is = delIgnore is xs
+            | otherwise = x : delIgnore is xs
+          readIgnore x = do
+            y <- doesFileExist x
+            if y then readF x else return ""
--- lib/GiveYouAHead/Help.hs
+++ lib/GiveYouAHead/Help.hs
@@ -12,15 +12,31 @@ module GiveYouAHead.Help
       import GiveYouAHead.Version(gyahver)
 
       helpInfo :: IO()
-      helpInfo = getProgName >>= (putStrLn.unlines.t)
+      helpInfo = getProgName >>= (putStrLn.unlines.helpAll)
         where
           helpAll pN = [
             "\n",
             "GiveYouAHead\t\t\t version "++gyahver,
             "\tUsage :",
             "\n",
-            "\t\t"++pN++" new (optional){-t [template] -d [directory?} [id/name] [the list of import]",
-            "",
+            "\tTo create a new file",
+            "\t\t"++pN++" new (optional){-t [template] -d [directory]} [id/name] [the list of import]",
+            "\n",
+            "\tTo initialize a new \"project\"",
+            "\t\t"++pN++" init (optional){-d [directory]}",
+            "\n",
+            "\tTo build something",
+            "\t\t"++pN++" build (optional){-t [template] -d [directory]} [the list of id/name]",
+            "\n",
+            "\tTo clean the temporary files",
+            "\t\t"++pN++" clean (optional){-t [template]}",
+            "\n",
+            "\tTo configure",
+            "\t\t"++pN++" config *undefined*",
+            "\n\n",
+            "\tGiveYouAHead's repo : https://github.com/Qinka/GiveYouAHead",
+            "\tBug report: https://github.com/Qinka/GiveYouAHead/issues",
+            ""
             ]
           t progName = [
             "\n",
@@ -35,4 +51,3 @@ module GiveYouAHead.Help
             "\t\t"++progName++" clean",
             ""
             ]
-     
