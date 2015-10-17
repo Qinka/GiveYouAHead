02c1053a55aee0bc837f2f69fcec5db3f0a2040a
Qinka <qinka@live.com>
2015-10-17 22:55:17 +0800

    add parsec for read extra option

--- lib/GiveYouAHead/Build/File.hs
+++ lib/GiveYouAHead/Build/File.hs
@@ -9,10 +9,47 @@ module GiveYouAHead.Build.File
     getOptions,
     getOptEnd,
     getOptBegin,
-    getOptionsFromFile
+    getOptionsFromFile,
+    eoBE,
+    eoEE,
+    extraOptionGet,
+    emptyE
     ) where
 
       import GiveYouAHead.Common(readF)
+      import Control.Monad
+      import Text.Parsec
+
+      extraOptionGet ::  (String,String,String) -> String -> Either ParseError String
+      extraOptionGet (a,b,c) = parse (eoBE a b c <|> emptyE a b c) "error" 
+
+      eoBE :: String -> String -> String -> Parsec  String () String -- commitmark,,ob oe
+      eoBE c b e = do
+        st <- string c *> many (noneOf "\n") <* char '\n'
+        if st == b then
+            liftM concat $ many $ eoEE c b e
+          else
+            liftM concat $ many $ eoBE c b e
+
+      emptyE :: String -> String -> String -> Parsec  String () String
+      emptyE c b e =do
+        _ <- many (noneOf "\n")  <* char '\n'
+        other <- many $ eoBE c b e <|> (many (noneOf "\n")  <* char '\n')
+        return $ concat other
+
+
+      eoEE :: String -> String -> String -> Parsec String () String
+      eoEE c b e = do
+        st <- string c *> many (noneOf "\n") <* char '\n'
+        if st == e then em else el st
+        where
+          em = do
+            _ <- many anyChar
+            return ""
+          el st= do
+            other <- many $ eoEE c b e <|> emptyE c b e
+            return $ concat $ st:other
+
 
 
       getFilesList :: String       -- file extension
