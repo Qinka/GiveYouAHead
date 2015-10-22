7d77452879693c0c45816837adbc343573cb8fda
Qinka <qinka@live.com>
2015-10-22 15:14:28 +0200

    fix the bug when you include the file itself. And when you include a file twice, it will be included once.

--- lib/Macro/MacroIO.hs
+++ lib/Macro/MacroIO.hs
@@ -14,11 +14,16 @@ module Macro.MacroIO
       import Control.Monad(liftM)
       import Macro.MacroParser(MacroNode(..),toMacro)
       import GiveYouAHead.Common(readF,getDataDir)
+      import Data.List(nub)
 
 
       getMacroFromFile :: FilePath -> IO [MacroNode]
-      getMacroFromFile fn' =
-        (>>= getInclude.toMacro.unlines.map (takeWhile (/='%')).lines) $ (>>= readF) fn
+      getMacroFromFile = (>>= return.snd).getMacroFromFileStep []
+
+
+      getMacroFromFileStep :: [String] -> FilePath -> IO ([String],[MacroNode])
+      getMacroFromFileStep list fn' =
+        (>>= getInclude list.toMacro.unlines.map (takeWhile (/='%')).lines) $ (>>= readF) fn
         where
           fn = if filehead == "global" then fnb else fna
             where
@@ -44,11 +49,13 @@ module Macro.MacroIO
                   else
                     error $ "macro: can not find file : "++fn'
 
-      getInclude :: [MacroNode] -> IO [MacroNode]
-      getInclude [] = return []
-      getInclude (Include fn:ms) = do
-        im <- getMacroFromFile fn
-        nm <- getInclude ms
-        return $ im++nm
-      getInclude (m:ms) =
-        liftM (m:) $ getInclude ms
+      getInclude :: [String] -> [MacroNode] -> IO ([String],[MacroNode])
+      getInclude list [] = return (list,[])
+      getInclude list (Include fn:ms)
+        | fn `elem` list = getInclude list ms
+        | otherwise = do
+          (list',im) <- getMacroFromFileStep (fn:list) fn
+          (l,nm) <- getInclude list' ms
+          return (nub $ list'++l,im++nm)
+      getInclude list (m:ms) =
+        liftM (\(x,y)->(x,m:y)) $ getInclude list ms
